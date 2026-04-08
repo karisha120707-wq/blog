3aadd50cef7baa8ebebd49a94d74ad8cefd5138e
Kim Karina <Kim@gmail.com>
2026-04-08 11:11:45 +0700

    Теперь добавлены значения по умолчанию для баланса пользователя

--- blog/login/models.py
+++ blog/login/models.py
@@ -7,5 +7,5 @@ class User (models.Model):
     first_name = models.CharField (max_length=200)
     last_name = models.CharField (max_length=200)
     age = models.PositiveBigIntegerField
-    balance = models.FloatField()
-    
+    balance = models.FloatField(default=0.0)
+
