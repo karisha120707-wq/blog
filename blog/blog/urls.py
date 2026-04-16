18b54751850b793bf1cd7c24a716c154390c61a4
Karina Kim <karisha120707@gmail.com>
2026-04-16 14:12:40 +0700

    Сделана авторизация

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -19,7 +19,11 @@ from django.urls import path
 from login import views
 
 urlpatterns = [
+    path('', views.index),
     path('add_user/', views.add_user), 
     path('users/', views.users),
     path('add_role/', views.add_role),
+    path('login/', views.login),
+    path('logout/', views.logout_view),
+
 ]
--- blog/login/views.py
+++ blog/login/views.py
@@ -2,6 +2,41 @@ from django.shortcuts import render, redirect
 from .forms import UserForm, RoleForm
 from .models import User
 
+
+def index (request):
+    if request.session.get('user_id'):
+        u_id = request.session.get('user_id')
+        u = User.objects.get(id = u_id)
+        return render (request, 'index.html', {'user': u})
+    else :
+        return redirect ('/login/')
+    
+
+def logout_view (request):
+    request.session.flush()
+    return redirect ('/login')
+
+
+def login (request) :
+    if request.method == "GET":
+        return render (request, 'login.html')
+    else:
+        login = request.POST.get('login')
+        password = request.POST.get('pas')
+
+        try:
+            user = User.objects.get(login=login)
+        except User.DoesNotExist:
+            return redirect ('/login')
+        
+    if password != user.password:
+        return redirect ('/login')
+    
+
+    request.session['user_id'] = user.id
+    request.session['login'] = user.login
+    return redirect ('/')
+
 def add_role (request):
     if request.method == "POST":
         role = RoleForm(request.POST)
@@ -13,8 +48,16 @@ def add_role (request):
         return render (request, "add_user.html", {'form': form})
 
 def users (request):
-    users = User.objects.all()
-    return render (request, 'users.html', {'users': users})
+    u = None
+    if request.session.get('user_id'):
+        u_id = request.session.get('user_id')
+        u = User.objects.get(id = u_id)
+
+        users = User.objects.all()
+        return render (request, 'users.html', {'users': users, 'user': u})
+    else :
+        return redirect ('/login/')
+
 
 def add_user(request):
     if request.method == "POST":
